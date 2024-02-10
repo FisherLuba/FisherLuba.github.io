70d21280a009490ff2a94d0d9e6cc6262a3e15fa
Fisher Luba <145061313+FisherLuba@users.noreply.github.com>
2024-02-10 00:02:21 -0500

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,44 @@
+// The best code you have ever seen
+
 let currentPercent = 10;
+let currentPercentBlue = 10;
 
 
 function doGradientCycle() {
     const gradientElements = document.getElementsByClassName("gradient-text");
     if (gradientElements == null) return;
     for (let element of gradientElements) {
-        // let value = `-webkit-linear-gradient(0deg, yellow, orange ${currentPercent}%, yellow ${100 - currentPercent}%)`;
-        // let value = `-webkit-linear-gradient(0deg, yellow 45%, orange 50%, yellow 55%)`;
         let value = `-webkit-linear-gradient(0deg, yellow ${currentPercent}%, orange ${currentPercent + 15}%, yellow ${currentPercent + 30}%)`;
-        // value = "-webkit-linear-gradient(0deg, orange $1, yellow 99)";
         element.style.background = value;
         console.log(element.style.background);
         element.style.color = "blue";
         element.style.webkitBackgroundClip = "text";
         element.style.webkitTextFillColor = "transparent";
-        // console.log(element.style);
     }
     if (currentPercent >= 55) {
         currentPercent = 10;
         return;
     }
-    currentPercent += 3.25;
+    currentPercent += 1.75;
 }
 
-setInterval(doGradientCycle, 100, 100);
+function doBlueGradientCycle() {
+    const gradientElements = document.getElementsByClassName("gradient-text-blue");
+    if (gradientElements == null) return;
+    for (let element of gradientElements) {
+        let value = `-webkit-linear-gradient(0deg, #2beaff ${currentPercentBlue}%, #209af7 ${currentPercentBlue + 15}%, #2beaff ${currentPercentBlue + 30}%)`;
+        element.style.background = value;
+        console.log(element.style.background);
+        element.style.color = "blue";
+        element.style.webkitBackgroundClip = "text";
+        element.style.webkitTextFillColor = "transparent";
+    }
+    if (currentPercentBlue >= 65) {
+        currentPercentBlue = 0;
+        return;
+    }
+    currentPercentBlue += 0.75;
+}
+
+setInterval(doGradientCycle, 50, 50);
+setInterval(doBlueGradientCycle, 50, 50);
